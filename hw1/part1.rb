e7eb2e056efdaf6453bf455d4f80687335e29dc1
Forest Monsen <forest.monsen@gmail.com>
2013-01-26 14:58:22 -0800

    Remove comments

--- hw1/part1.rb
+++ hw1/part1.rb
@@ -1,8 +1,3 @@
-#For those just starting out, one suggested way to work on your code is to have a command window open and a text editor with this
-#file loaded.  Make changes to this file and then run 'ruby part1.rb' at the command line, this will run your program.  Once you're
-#satisfied with your work, save your file and upload it to the checker.
-
-
 def palindrome?(str)
     forward = str.downcase.gsub(/[\W\s]/, '')
     reverse = str.reverse.downcase.gsub(/[\W\s]/, '')
