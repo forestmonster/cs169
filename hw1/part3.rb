55bf1c92ae8079bbcd8ac70b57731fccde242574
Forest Monsen <forest.monsen@gmail.com>
2013-01-27 00:11:52 -0800

    Anagrams coming up

--- hw1/part3.rb
+++ hw1/part3.rb
@@ -1,3 +1,9 @@
+# Write a method combine_anagrams(words) that, given an array of strings words,
+# groups the input words into anagram groups. Case doesn't matter in
+# classifying strings as anagrams (but case should be preserved in the output),
+# and the order of the anagrams in the groups doesn't matter. The output should
+# be an array of anagram groups (i.e. an array of arrays).
+
 def combine_anagrams(words)
  # YOUR CODE HERE
 end
