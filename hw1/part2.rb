440d6dbeca38dc82030395c0c0cd222d9e322dd0
Forest Monsen <forest.monsen@gmail.com>
2013-01-26 19:05:05 -0800

    Added exception handling

--- hw1/part2.rb
+++ hw1/part2.rb
@@ -7,6 +7,8 @@ end
 
 def rps_game_winner(game)
   raise WrongNumberOfPlayersError unless game.length == 2
+  raise NoSuchStrategyError unless /^[RPSrps]{1}$/.match(game[0][1])
+  raise NoSuchStrategyError unless /^[RPSrps]{1}$/.match(game[1][1])
 end
 
 def rps_tournament_winner(tournament)
@@ -19,4 +21,4 @@ end
 # Otherwise, return the name and move of the winning player. If both players
 # play the same move, the first player is the winner.
 
-rps_game_winner [ ["Armando", "P"], ["Dave", "S"] ]
+rps_game_winner [ ["Armando", "T"], ["Dave", "S"] ]
