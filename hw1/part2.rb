33f0a2618a7944bfee37b1a43c1d0f93d583fff9
Forest Monsen <forest.monsen@gmail.com>
2013-01-26 18:42:39 -0800

    Getting started

--- hw1/part2.rb
+++ hw1/part2.rb
@@ -6,9 +6,17 @@ def rps_result(m1, m2)
 end
 
 def rps_game_winner(game)
-  # YOUR CODE HERE
+  raise WrongNumberOfPlayersError unless game.length == 2
 end
 
 def rps_tournament_winner(tournament)
   # YOUR CODE HERE
 end
+
+# If the number of players is not equal to 2, raise WrongNumberOfPlayersError.
+# If either player's strategy is something other than "R", "P" or "S"
+# (case-insensitive), raise NoSuchStrategyError.
+# Otherwise, return the name and move of the winning player. If both players
+# play the same move, the first player is the winner.
+
+rps_game_winner [ ["Armando", "P"], ["Dave", "S"] ]
