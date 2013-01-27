ae729868cb7f6f0b90dda0316f88ff41624eb463
Forest Monsen <forest.monsen@gmail.com>
2013-01-27 12:20:16 -0800

    Trying a couple of arrays.

--- hw1/part3.rb
+++ hw1/part3.rb
@@ -6,11 +6,26 @@
 
 def combine_anagrams(words)
     # - Take each element of the array and sort its letters alphabetically,
-    #   without regard to case. Save each sorted element into a new array.
-    # - Save it into a new sub-array.
+    #   without regard to case.
+    # - Save each sorted element into a new sub-array.
+    # - Iterate through the new array, comparing each element to each other
+    #   element. If they match, save them into a new array together. Push any
+    #   later matches onto the end of that array.
     # - Compare it to every other member of the character-sorted array. If it
     #   matches another, append that one to the new sub-array.
- words.each { |w| w.chars}
+    # - For each one, compare it to the next element in the arry. If it doesn't
+    #   match, go on to the next one. If it does match, 
+  array = []
+  words.each do |w|
+    array << w.chars.sort.join
+    puts array.inspect
+    #w.chars.sort { |a, b| a.casecmp(b) } .join
+  end
+
+  array_container = []
+  array.each do |a|
+    a.pop.inspect
+  end
 end
 
-puts combine_anagrams(['cars', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream'])
+puts combine_anagrams(['cars', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream']).inspect
