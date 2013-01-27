70787ac66ca20c6d5d8c66c27c212bfac0d948c2
Forest Monsen <forest.monsen@gmail.com>
2013-01-26 19:31:06 -0800

    Removing some comments

--- hw1/part2.rb
+++ hw1/part2.rb
@@ -1,45 +1,12 @@
 class WrongNumberOfPlayersError < StandardError ; end
 class NoSuchStrategyError < StandardError ; end
 
-def rps_result(m1, m2)
-  name01 = m1[0][0]
-  name02 = m2[1][0]
-  play_one = m1[0][1]
-  play_two = m2[1][1]
-  # Player One Rock
-  if play_one == 'R' && play_two == "R"
-    name01
-  elsif play_one == 'R' && play_two == "P"
-    name02
-  elsif play_one == 'R' && play_two == "S"
-    name01
-  # Player One Paper
-  elsif play_one == 'P' && play_two == "R"
-    name01
-  elsif play_one == 'P' && play_two == "P"
-    name01
-  elsif play_one == 'P' && play_two == "S"
-    name02
-  # Player One Scissors
-  elsif play_one == 'S' && play_two == "R"
-    name02
-  elsif play_one == 'S' && play_two == "P"
-    name02
-  elsif play_one == 'S' && play_two == "S"
-    name01
-  else
-    name01
-  end
-end
-
 def rps_game_winner(game)
   raise WrongNumberOfPlayersError unless game.length == 2
   raise NoSuchStrategyError unless /^[RPSrps]{1}$/.match(game[0][1])
   raise NoSuchStrategyError unless /^[RPSrps]{1}$/.match(game[1][1])
   game[0][1].upcase!
   game[1][1].upcase!
-
-  puts "Game is a" + game.inspect
 
   name01 = game[0][0]
   name02 = game[1][0]
