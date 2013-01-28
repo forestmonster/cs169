5fda66e579f36167d16e69ad5e538c027d7c2763
Forest Monsen <forest.monsen@gmail.com>
2013-01-27 17:04:19 -0800

    New strategy

--- hw1/part3.rb
+++ hw1/part3.rb
@@ -4,28 +4,32 @@
 # and the order of the anagrams in the groups doesn't matter. The output should
 # be an array of anagram groups (i.e. an array of arrays).
 
-def combine_anagrams(words)
-    # - Take each element of the array and sort its letters alphabetically,
-    #   without regard to case.
-    # - Save each sorted element into a new sub-array.
-    # - Iterate through the new array, comparing each element to each other
-    #   element. If they match, save them into a new array together. Push any
-    #   later matches onto the end of that array.
-    # - Compare it to every other member of the character-sorted array. If it
-    #   matches another, append that one to the new sub-array.
-    # - For each one, compare it to the next element in the arry. If it doesn't
-    #   match, go on to the next one. If it does match, 
-  array = []
-  words.each do |w|
-    array << w.chars.sort.join
-    puts array.inspect
-    #w.chars.sort { |a, b| a.casecmp(b) } .join
-  end
+# You're given a flat array of words.
+# Start with an empty hash.
+# For each word in the array of words, sort it by character (case-
+# insensitively) into an enumerable, then join it back into a single
+# word.
+# Search for the word in your hash. Is it already there?
+# If it isn't, initialize a new array, using the sorted letters
+# as a key, to point to the new array as a value. Otherwise continue.
+# Whether it is or isn't, assign the original word to the value arr.
+# Use the length of "arr" as a key, to point to the original word.
+# Then reset arr to empty.
 
-  array_container = []
-  array.each do |a|
-    a.pop.inspect
+def combine_anagrams(words)
+  hash = Hash.new
+  words.each do |str|
+     sorted = str.chars.sort { |a, b| a.casecmp(b) } .join
+     if (hash[sorted]== nil)
+       hash[sorted] = Array.new
+     end
+     arr = hash[sorted]
+     arr[arr.length] = str
+     hash[sorted] = arr
+     puts "[sorted] is \"" + sorted + ",\" and hash[sorted] is " + hash[sorted].inspect
+     arr = nil
   end
+  hash.values
 end
 
-puts combine_anagrams(['cars', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream']).inspect
+puts combine_anagrams(['cars', 'SArc', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream', 'hello', 'HeLLo']).inspect
