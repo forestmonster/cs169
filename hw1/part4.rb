6aad584e1904a82fbaf0c5e3d9532ee0cfaf7952
Forest Monsen <forest.monsen@gmail.com>
2013-01-27 17:24:37 -0800

    Creating getter and setter methods

--- hw1/part4.rb
+++ hw1/part4.rb
@@ -2,13 +2,29 @@ class Dessert
   def initialize(name, calories)
     # YOUR CODE HERE
   end
-  
+
+  def setName(name)
+    @name = name
+  end
+
+  def setCalories(cal)
+    @calories = cal
+  end
+
+  def getName
+    return @name
+  end
+
+  def getCalories(cal)
+    return @calories
+  end
+
   def healthy?
     # YOUR CODE HERE
   end
   
   def delicious?
-    # YOUR CODE HERE
+    true
   end
 end
 
